d83ba253a3947d890700ef618e82c6e40641a89c
Guadeloupe33 <stevenfrancius@gmail.com>
2023-10-03 13:42:49 -0400

    added addtional route

--- controllers/api/cartRoute.js
+++ controllers/api/cartRoute.js
@@ -1,2 +1,8 @@
 const router = require('express').Router();
 const Dish = require('../models/Cart');
+
+
+router.get('/cart/:id', async (req, res) => {
+    try {
+        const dishData = await Dish.findByPk(req.params.id);
+        console.log(cartData)
