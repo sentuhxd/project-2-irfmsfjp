5f69cbe121ee9e739a883aba2acfe9d69c70d464
sentuh <142236429+sentuhxd@users.noreply.github.com>
2023-10-03 13:51:21 -0400

    fixed casing

--- models/index.js
+++ models/index.js
@@ -2,11 +2,13 @@ const User = require('./User');
 const Product = require('./Product');
 const Category = require('./Category');
 const Cart = require('./Cart');
-const CartItem = require('./cart-item');
+const CartItem = require('./Cart-Item');
 
 Product.belongsTo(Category);
 
 Category.hasMany(Product);
+
+
 
 
 
