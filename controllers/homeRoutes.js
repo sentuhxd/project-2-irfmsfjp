3651d69f46aa15ef674e0305a3f49a5ed492095c
Guadeloupe33 <stevenfrancius@gmail.com>
2023-10-03 11:58:55 -0400

    added loging router

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -9,3 +9,12 @@ router.get('/', withAuth, async (req, res) => {
         order: [['name', 'ASC']],
       });
       const users = userData.map((project) => project.get({ plain: true }));
+      res.render('homepage', {
+        users,
+        logged_in: req.session.logged_in,
+      });
+    } catch (err) {
+        res.status(500).json(err);
+      }
+    });
+    
