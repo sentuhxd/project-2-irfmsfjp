b8f9af53703af9759ed57583ca84a6e0b67e0f5d
sentuh <142236429+sentuhxd@users.noreply.github.com>
2023-10-03 13:35:55 -0400

    product belongs to category

--- models/index.js
+++ models/index.js
@@ -4,5 +4,12 @@ const Category = require('./Category');
 const Cart = require('./Cart');
 const CartItem = require('./cart-item');
 
+Product.belongsTo(Category);
+
+Category.hasMany(Product);
+
+
+
+
 
 module.exports = { User, Product, Category, Cart, CartItem } ;
