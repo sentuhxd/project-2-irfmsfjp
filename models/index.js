901318b39d5d18ae22d1a04e2245392f66838671
sentuh <142236429+sentuhxd@users.noreply.github.com>
2023-10-04 10:48:45 -0400

    worked on more relationships

--- models/index.js
+++ models/index.js
@@ -4,9 +4,42 @@ const Category = require('./Category');
 const Cart = require('./Cart');
 const CartItem = require('./Cart-Item');
 
-Product.belongsTo(Category);
+Product.belongsToMany(Category);
 
 Category.hasMany(Product);
+
+User.hasMany(Product);
+
+User.hasMany(CartItem, {
+    foreignKey: {name: 'userId', allowNull:false}, 
+    onDelete: 'CASCADE'
+});
+
+Product.belongsTo(Category, {
+    foreignKey: { name: 'categoryid', allowNull: false },
+    onDelete: 'CASCADE'
+});
+
+Category.hasMany(Product, {
+    foreignKey: { name: 'categoryid', allowNull: false },
+    onDelete: 'CASCADE'
+})
+
+Cart.belongsTo(User, {
+    foreignKey: {name: 'userid', allowNull: false},
+    onDelete: 'CASCADE'
+
+})
+
+Cart.hasMany(CartItem, {
+    foreignKey: {name: 'cartid', allowNull:false},
+    onDelete: 'CASCADE'
+
+})
+
+
+
+
 
 
 
