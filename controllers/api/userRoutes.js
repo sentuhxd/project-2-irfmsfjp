0911ae9d16571b377b11151887d30a3e074dbca2
pineonline10 <joshisstillpine@gmail.com>
2023-10-05 14:51:29 -0400

    passport FINALLY WORKSgit add -Agit add -A

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,15 +1,19 @@
 const router = require('express').Router();
-const passport = require('../config/passport-config'); // Require the configured Passport instance
-const { User } = require('../models');
+const passport = require('../../config/passport-config'); // Require the configured Passport instance
+const { User } = require('../../models');
 
 // Update the POST /login route to use Passport's authenticate method
 router.post('/login', passport.authenticate('local'), (req, res) => {
-  // This callback will only be called if authentication is successful
-  res.json({ user: req.user, message: 'You are now logged in!' });
+  req.session.save(() => {
+    req.session.loggedIn = true;
+    // This callback will only be called if authentication is successful
+    res.json({ user: req.user, message: 'You are now logged in!' });
+  })
+
 });
 
 router.post('/logout', (req, res) => {
-  if (req.session.logged_in) {
+  if (req.session.loggedIn) {
     req.session.destroy(() => {
       res.status(204).end();
     });
