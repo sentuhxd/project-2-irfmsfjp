7ca9d351103cb82fec8be4bd56c69ec7f3fef09b
pineonline10 <joshisstillpine@gmail.com>
2023-10-05 11:12:50 -0400

    adds passport.js to config for passport pkg

---
+++ config/passport.js
@@ -0,0 +1,24 @@
+// passport-config.js
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const User = require('../models/User');
+
+passport.use(
+  new LocalStrategy((username, password, done) => {
+    User.findOne({ where: { username } }).then((user) => {
+      if (!user) return done(null, false);
+      if (!user.validPassword(password)) return done(null, false);
+      return done(null, user);
+    });
+  })
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findByPk(id).then((user) => {
+    done(null, user);
+  });
+});
