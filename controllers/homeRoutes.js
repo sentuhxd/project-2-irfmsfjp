3a6b02c158e0fa3ba02d7aed43bcb5f79babb29e
sentuh <142236429+sentuhxd@users.noreply.github.com>
2023-10-06 12:41:05 -0400

    made cart modal

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,19 +1,37 @@
 const router = require('express').Router();
-const { Cart, Product, User } = require('../models'); 
+const { Cart, Product, CartItem } = require('../models'); 
 const withAuth = require('../utils/auth');
 
 router.get('/', withAuth, async (req, res) => {
     try {
       let currentUser = req.session.loggedIn;
-      console.log(currentUser)
+      let cart;
       let products = await Product.findAll();
+      if (currentUser) {
+        cart = await Cart.findOne({
+          include: [
+            { model: CartItem },
+            {
+              model: Product,
+              through: CartItem,
+              as: 'cart_products'
+            }
+          ],
+          where: {
+            user_id: user.id
+          }
+        })
+      } 
+
+      // process cartitems and cart_products
 
       // handlebars does not like sequalize model objects
       products = JSON.parse(JSON.stringify(products)); // plain old Javascript objects
-
+      console.log('cart', cart);
       res.render('homepage', {
         loggedIn: currentUser,
-        products
+        products,
+        cart
       });
     } catch (err) {
       console.log("error", err)
@@ -32,6 +50,8 @@ router.get('/login', (req, res) => {
 router.get('/signup', (req, res) => {
   res.render('signup');
 })
+
+
 
 module.exports = router;
     
