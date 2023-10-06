27b2b0ec65455fae8d1ff378bc1c91cf86273f5f
sentuh <142236429+sentuhxd@users.noreply.github.com>
2023-10-06 10:15:04 -0400

    render stuff on page still needing image to render

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,34 +1,36 @@
 const router = require('express').Router();
-const { User } = require('../models'); 
+const { Cart, Product, User } = require('../models'); 
 const withAuth = require('../utils/auth');
 
 router.get('/', withAuth, async (req, res) => {
     try {
-      const userData = await User.findAll({
-        attributes: { exclude: ['password'] },
-        order: [['name', 'ASC']],
-      });
-      const users = userData.map((project) => project.get({ plain: true }));
+      let currentUser = req.session.logged_in;
+      let products = await Product.findAll();
+
+      // handlebars does not like sequalize model objects
+      products = JSON.parse(JSON.stringify(products)); // plain old Javascript objects
+
       res.render('homepage', {
-        users,
-        logged_in: req.session.logged_in,
+        logged_in: currentUser,
+        products
       });
     } catch (err) {
-        res.status(500).json(err);
-      }
-    });
+      console.log("error", err)
+      res.status(500).json(err);
+    }
+});
     
-    router.get('/login', (req, res) => {
-        if (req.session.logged_in) {
-          res.redirect('/');
-          return;
-        }
-        res.render('login');
-    });
+router.get('/login', (req, res) => {
+    if (req.session.logged_in) {
+      res.redirect('/');
+      return;
+    }
+    res.render('login');
+});
 
-    router.get('/signup', (req, res) => {
-      res.render('signup');
-    })
-    
-    module.exports = router;
+router.get('/signup', (req, res) => {
+  res.render('signup');
+})
+
+module.exports = router;
     
