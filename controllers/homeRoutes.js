44e84b1a5a7a999a258080e07e93d756ae983284
sentuh <142236429+sentuhxd@users.noreply.github.com>
2023-10-06 14:50:54 -0400

    fixed merge conflicts

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -50,7 +50,6 @@ router.get('/signup', (req, res) => {
   res.render('signup');
 })
 
-<<<<<<< HEAD
 router.get('/user-store', (req, res) => {
   res.render('user-store'); 
 });
@@ -58,9 +57,6 @@ router.get('/user-store', (req, res) => {
 router.get('/user', (req, res) => {
   res.render('user'); 
 });
-=======
-
->>>>>>> 79d14babdbd3272fbc4151c2dfb65fc009961dc9
 
 module.exports = router;
     
