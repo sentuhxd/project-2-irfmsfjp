ad99b07e3ad6b812a3fcfe27ebb388d7f6aa7607
Guadeloupe33 <stevenfrancius@gmail.com>
2023-10-10 08:10:56 -0400

    added route

---
+++ controllers/stripeRoute.js
@@ -0,0 +1,4 @@
+const express = require('express').router();
+const router = require('../apiRoutes');
+const router = express();
+const stripe = require('stripe')('sk_test_51NxZSzBVsAs3ng48C78qHNSsEzS3qvrxGfiSiFyBD8ILdrPunnnppuSCKXj8qQADt9m9oAM65Rp4B75Y2HlbCGB900enyCSmTV');
